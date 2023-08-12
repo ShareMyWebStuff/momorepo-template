34551e960ad925fe0728af1768698aee94cab5d1
Dave Ferguson <dave@harmonydata.co.uk>
2023-08-12 11:36:36 +0100

    Changed lambda function name

--- apps/infrastructure/lib/cdk-stack.ts
+++ apps/infrastructure/lib/cdk-stack.ts
@@ -14,14 +14,15 @@ export class DatabaseDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps, buildConfig: BuildConfig) {
     super(scope, id, props);
 
-    const vpcName = buildConfig.Prefix + '-' + buildConfig.Environment+'-vpc'
+    // const vpcName = buildConfig.Prefix + '-' + buildConfig.Environment+'-vpc'
 
-      // 👇 define GET todos function
-      const getTodosLambda = new lambda.Function(this, 'deploy-database-changeset', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: 'index.main',
-      code: lambda.Code.fromAsset(path.join(__dirname, '/../src/deploy-database-changeset')),
-      });
+    // 👇 define deploy database changeset
+    const deployDatabaseChangesetLambda = new lambda.Function(this, buildConfig.Prefix + '-' + buildConfig.Environment + '-deploy-database-changeset', {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    handler: 'index.main',
+    functionName: buildConfig.Prefix + '-' + buildConfig.Environment + '-deploy-database-changeset',
+    code: lambda.Code.fromAsset(path.join(__dirname, '/../src/deploy-database-changeset')),
+    });
   
   }
 
